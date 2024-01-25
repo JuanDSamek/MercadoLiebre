78e3e9adb43154993cbcedfde4637747b5654bd1
JuanDSamek <juanelpato04@gmail.com>
2024-01-25 00:11:18 -0300

    Armando formulario

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ app.listen(port, () => console.log("http://localhost:3030"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+app.get("/registro",(req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+app.get("/login",(req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
